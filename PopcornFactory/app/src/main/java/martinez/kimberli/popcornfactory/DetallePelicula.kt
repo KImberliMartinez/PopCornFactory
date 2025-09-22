8e17aecfa2bec70d74783844026554a68ac79d0d
KImberliMartinez <159958568+KImberliMartinez@users.noreply.github.com>
2025-09-22 02:15:35 -0700

    inicial

---
+++ PopcornFactory/app/src/main/java/martinez/kimberli/popcornfactory/DetallePelicula.kt
@@ -0,0 +1,27 @@
+package martinez.kimberli.popcornfactory
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class DetallePelicula : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_detalle_pelicula)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val bundle=intent.extras
+
+        if (bundle!=null){
+            iv_pelicula_imagen.setImageResource(bundle.getInt("header"))
+            tv_nombre_pelicula.setText(bundle.getString("nombre"))
+            tv_pelicula_desc.setText(bundle.getString("sinopsis"))
+        }
+    }
+}
---
+++ PopcornFactory/app/src/main/java/martinez/kimberli/popcornfactory/Pelicula.kt
@@ -0,0 +1,6 @@
+package martinez.kimberli.popcornfactory
+
+data class Pelicula(var titulo: String,
+                    var image: Int,
+                    var header: Int,
+                    var sinopsis: String)
